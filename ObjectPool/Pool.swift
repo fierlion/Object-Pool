c04bcbdef5ebecaad0c9e2456fc10d9ebaa4b2ab
fierlion <fierlion@gmail.com>
2015-12-15 18:57:43 -0800

    added GCD queue and semaphore

--- ObjectPool/Pool.swift
+++ ObjectPool/Pool.swift
@@ -11,23 +11,33 @@ import Foundation
 class Pool<T> {
     // the pool of objects is a queue
     private var objectPool = [T]()
+    // ensure that no two threads use methods getFrom() and returnTo() simultaneously
+    private let arrayQ = dispatch_queue_create("arrayQ", DISPATCH_QUEUE_SERIAL)
+    // use semaphore as items.count conditional for getFromPool
+    private let semaphore:dispatch_semaphore_t
     
     init(items:[T]) {
         objectPool.reserveCapacity(objectPool.count)
         for item in items {
             objectPool.append(item)
         }
+        semaphore = dispatch_semaphore_create(items.count)
     }
     
     func getFromPool() -> T? {
         var result:T?
-        if objectPool.count > 0 {
-            result = self.objectPool.removeAtIndex(0)
+        if (dispatch_semaphore_wait(semaphore, DISPATCH_TIME_FOREVER) == 0) {
+            dispatch_sync(arrayQ, {() in
+                result = self.objectPool.removeAtIndex(0)
+            })
         }
         return result
     }
     
     func returnToPool(item:T) {
-        self.objectPool.append(item)
+        dispatch_async(arrayQ, {()
+            self.objectPool.append(item)
+            dispatch_semaphore_signal(self.semaphore)
+        })
     }
 }
